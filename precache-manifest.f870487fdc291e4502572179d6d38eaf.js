2900a76a658f71fe46d05bca718379ce473afc5a
CHUN-BYUNGJAE <0167827052@naver.com>
2020-08-21 16:12:51 +0900

    Updates

---
+++ precache-manifest.f870487fdc291e4502572179d6d38eaf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e38d753aa1c829a3ce864b044fd4bb50",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "ba56f6777d45712f58a6",
+    "url": "/movie_app_2020/static/css/main.551862d6.chunk.css"
+  },
+  {
+    "revision": "3dd18e87ab088f0503d7",
+    "url": "/movie_app_2020/static/js/2.56ed097a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.56ed097a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba56f6777d45712f58a6",
+    "url": "/movie_app_2020/static/js/main.a61fe867.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
